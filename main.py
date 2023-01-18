e8a5f2670dadf4e0527903b40aa8c0d8bed91826
xXTechPotatoXx <nur.ashsams@gmail.com>
2023-01-18 13:17:15 -0500

    Blurring and Morhpohlogical Transformations

--- main.py
+++ main.py
@@ -81,6 +81,72 @@ import shutil  # to save it loca
 
 #_______________________________________________________________________________________________________________________
 
+## Blur
+
+
+# img = cv.imread("C:/Users/nuras/Pictures/ProjectPictures/mint.png")
+# cv.imwrite("mint.png", img)
+# cv.imshow("mint", img)
+# cv.waitKey(0)
+
+# img = cv.imread('mint.png')
+
+# The parameters are, neighborhood diameter, sigmaColor, sigmaSpace
+# each of these are essentially determining how the kernel used to apply the filter is being used
+# diameter for size, color for color range, and space for the percentage further pixel in the neighborhood affect
+# blur = cv.bilateralFilter(img,20,75,75)
+
+# cv.imwrite("blurmint.png", blur)
+# cv.imshow("blur", blur)
+# cv.waitKey(0)
+
+
+#_______________________________________________________________________________________________________________________
+
+## Morphological Transformations: Erosion, Dilation, Etc
+
+
+img = cv.imread('binmint.png')
+kernel = np.ones((5,5),np.uint8)
+
+#1
+erosion = cv.erode(img,kernel,iterations = 1)
+cv.imshow("Erosion", erosion)
+cv.waitKey(0)
+
+#2
+dilation = cv.dilate(img,kernel,iterations = 1)
+cv.imshow("Dilation", dilation)
+cv.waitKey(0)
+
+#3
+opening = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+cv.imshow("Opening", opening)
+cv.waitKey(0)
+
+#4
+closing = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+cv.imshow("Closing", closing)
+cv.waitKey(0)
+
+#5
+gradient = cv.morphologyEx(img, cv.MORPH_GRADIENT, kernel)
+cv.imshow("Gradient", gradient)
+cv.waitKey(0)
+
+#6
+tophat = cv.morphologyEx(img, cv.MORPH_TOPHAT, kernel)
+cv.imshow("Top Hat", tophat)
+cv.waitKey(0)
+
+# #7
+# blackhat = cv.morphologyEx(img, cv.MORPH_BLACKHAT, kernel)
+# cv.imshow("Black Hat", blackhat)
+# cv.waitKey(0)
+
+#_______________________________________________________________________________________________________________________
+
+## Countouring on non blurred and eroded images
 
 # img = cv.imread("C:/Users/nuras/Pictures/ProjectPictures/mint.png")
 # cv.imwrite("mint.png", img)
@@ -88,19 +154,34 @@ import shutil  # to save it loca
 # cv.waitKey(0)
 
 
-img = cv.imread('mint.png')
-imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow('imgray', imgray)
-ret, thresh = cv.threshold(imgray, 190, 255, 0)
-print(thresh)
-cv.imshow("Thresh", thresh)
-cv.waitKey(0)
-contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-cont = cv.drawContours(img, contours, -1, (0,255,0), 3)
-cv.imshow("contours", cont)
-cv.waitKey(0)
+# img = cv.imread('mint.png')
+# imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('imgray', imgray)
+# ret, thresh = cv.threshold(imgray, 190, 255, 0)
+# print(thresh)
+# cv.imwrite("binmint.png", thresh)
+# cv.imshow("Thresh", thresh)
+# cv.waitKey(0)
+# contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# cont = cv.drawContours(img, contours, -1, (0,255,0), 2)
+# cv.imshow("contours", cont)
+# cv.waitKey(0)
 
 
+## The following reads the initial image with a single color space and output a read image that is already gray
+
+# img = cv.imread('mint.png', 0)
+# cv.imshow('imgray', img)
+# ret, thresh = cv.threshold(img, 190, 255, 0)
+# print(thresh)
+# cv.imshow("Thresh", thresh)
+# cv.waitKey(0)
+# contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# cont = cv.drawContours(img, contours, -1, (0,255,0), 2)
+# cv.imshow("contours", cont)
+# cv.waitKey(0)
+
+#_______________________________________________________________________________________________________________________
 
 
 
